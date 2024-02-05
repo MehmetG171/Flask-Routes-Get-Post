14167012ed14f3e23fd61c112df0896b98cb5855
MehmetG171 <68816334+MehmetG171@users.noreply.github.com>
2024-02-05 21:08:07 +0300

    Final Commit

--- flask-handling-forms/flask-handling-forms/app.py
+++ flask-handling-forms/flask-handling-forms/app.py
@@ -1,35 +1,43 @@
 # Import Flask modules
-from flask import Flask, render_template, request
+from flask import Flask, redirect, url_for, render_template, request
+
 # Create an object named app
 app = Flask(__name__)
 
-# create a function named "lcm" which calculates a least common multiple values of two numbers. 
-def lcm(num1,num2):
-    common_multiplications = [] 
-    for i in range(max(num1, num2), num1 * num2 + 1):
-        if i % num1 == 0 and i % num2 == 0:
-           common_multiplications.append(i) 
-    return min(common_multiplications)
 
-# Create a function named `index` which uses template file named `index.html` 
-# send two numbers as template variable to the app.py and assign route of no path ('/') 
-@app.route("/")
-def index():
-    return render_template("index.html", methods = ["GET"])
+# Create welcome page with main.html file and assign it to the root path
+@app.route('/')
+def home():
+    return render_template('main.html', name = 'Mehmet')
 
-# calculate sum of them using "lcm" function, then sent the result to the 
-# "result.hmtl" file and assign route of path ('/calc'). 
-# When the user comes directly "/calc" path, "Since this is a GET request, LCM has not been calculated" string returns to them with "result.html" file
-@app.route("/calc", methods = ["GET", "POST"])
-def calculate():
-    if request.method == "POST":
-        num1 = request.form.get("number1")
-        num2 = request.form.get("number2")
-        return render_template("result.html", result1 = num1, result2 = num2, lcm = lcm(int(num1), int(num2)), developer_name = 'Mehmet')
+# Write a function named `greet` which uses template file named `greet.html` given under 
+# `templates` folder. it takes parameters from query string on URL, assign that parameter 
+# to the 'user' variable and sent that user name into the html file. If it doesn't have any parameter, warning massage is raised
+
+@app.route('/greet', methods=['GET'])
+def greet(): 
+   if 'user' in request.args: 
+        usr = request.args['user']
+        return render_template('greet.html', user = usr)
+   else:
+        return render_template('greet.html', user='Send your user name with "greet?user=xxxxx" param in query string')
+
+# Write a function named `login` which uses `GET` and `POST` methods, 
+# and template files named `login.html` and `secure.html` given under `templates` folder 
+# and assign to the static route of ('login')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        user_name = request.form['username']
+        password = request.form['password']
+        if password == 'python':
+            return render_template('secure.html', user = user_name.title())
+        else:
+            return render_template('login.html', user = user_name.title(), control = True)
     else:
-        return render_template("result.html", developer_name = "Mehmet")
+        return render_template('login.html', control = False)
 
-# Add a statement to run the Flask application which can be debugged.
-if __name__== "__main__":
+# Add a statement to run the Flask application which can be reached from any host on port 80.
+if __name__ == '__main__':
     app.run(debug = True)
     # app.run(host='0.0.0.0', port=8081)
